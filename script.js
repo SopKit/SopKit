ffa6bd1ae9920dabb21df798ec52f469d9fdf9bf
sh20raj1 <160089473+sh20raj1@users.noreply.github.com>
2024-02-27 20:45:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,5 +42,7 @@ var url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS7EF_2Yz5C_RbVKsSmOj
 
   // Add event listener to trigger search on keyup event in the search input
   document.getElementById("search").addEventListener("keyup", search);
+  document.addEventListener("scroll", search);
+  document.onload = search;
+
 })();
-search();
